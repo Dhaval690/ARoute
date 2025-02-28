f47e5bd39e2192dcdf37e9f8a74d866a38ad5e27
fatswag <fsalam@uclan.ac.uk>
2025-02-28 11:16:26 +0000

    HIIIIII
    Its me🦑

---
+++ ARoute/Assets/Scripts/LocationPointerSelect.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LocationPointerSelect : MonoBehaviour
+{
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+        Debug.Log("Application has quit");
+    }
+
+}
